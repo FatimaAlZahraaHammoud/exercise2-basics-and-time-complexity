fd1d357fa7e40e962723b02e5fdbaccc9d86b3ae
unknown <fatimaalzahraahammoud@gmail.com>
2024-10-27 20:53:03 +0200

    Conditions Added

--- exercise2-pyramid-of-odd-numbers.js
+++ exercise2-pyramid-of-odd-numbers.js
@@ -1,4 +1,8 @@
 var x = parseInt(prompt("Enter a number"));
+while(x <= 0){
+    alert("Please enter a valid positive integer: ");
+    x = parseInt(prompt("Enter the number of rows:"));
+}
 
 for (var i = 0; i <= x; i++){
     var num = 1;
--- exercise2-pyramid-of-odd-numbers.py
+++ exercise2-pyramid-of-odd-numbers.py
@@ -1,5 +1,9 @@
 x = int(input("Enter a number: "))
 
+while x <= 0:
+    print("Please enter a valid positive integer!")
+    x = int(input("Enter a number: "))
+
 for i in range(x):
     num = 1
     for j in range(i + 1):
